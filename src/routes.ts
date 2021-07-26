ee71229ccb10c1e032a9f2bbcd2a5bcc505afa13
Johan <johan@macair2020>
2021-07-26 23:00:15 +0200

    init

--- src/routes.ts
+++ src/routes.ts
@@ -19,6 +19,11 @@ const ssoRoutes = <A, B = any>(
 
   router.get(paths.redirect, async (ctx) => {
     const { code } = ctx.request.query;
+
+    if (!code || typeof code !== "string") {
+      throw Error("code is not of the right type");
+    }
+
     const token = await SSO.callback(code);
     const profile: A = await SSO.getProfile(token);
     ctx.body = loginAuthenticate ? await loginAuthenticate(profile) : profile;
